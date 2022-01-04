f3f9403f6af7b2f5886a3d62f76afe7cf4e836c7
Eloi <recycled_loops@hotmail.com>
2022-01-04 20:59:55 +0100

    create task

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,6 +2,23 @@ pragma solidity ^0.5.0;
 
 
 contract TodoList {
-    uint taskCount = 0;
+    uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    constructor() public {
+        createTask("My TODO list Dapp! :)");
+    }
+
+    mapping(uint => Task) public tasks;
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
 
